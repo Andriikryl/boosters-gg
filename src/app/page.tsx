215a3c6b899ed3d0f86c62cd28c129e6b1fa126f
Andriikryl <andreyk557@gmail.com>
2023-08-31 15:01:34 +0300

    git init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum adipisci reiciendis qui accusamus optio aliquid ut, quo fugiat voluptatum maiores placeat repudiandae. Recusandae enim cupiditate tenetur perferendis tempore iste consequuntur.</p>
+    </div>
+  )
+}
